cb66207b4142b1b5a986e331e9f3acf0da68b222
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-09-27 12:48:32 +0200

    problem with changeSlide function

--- js/script.js
+++ js/script.js
@@ -2,14 +2,26 @@
 
 $(function(){
 	var carouselList = $('#carousel ul');
-	$(document).changeSlide(function(){
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  });
-	setInterval(changeSlide, 3000);
 
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
+	changeSlide(function(){
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	});
 
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
+	setInterval(function(){
+    	changeSlide();},3000);
+
+	moveFirstSlide(function() { 
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+	    lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	});
 });
+/*
+$('p').hide();
+  $('button').click(function(){
+    $('p').slideToggle();
+  });
+}
+setInterval(function(){ alert("Hello"); }, 3000);
+*/
