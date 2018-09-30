652499d341d97b4e47d3c9fb7d278a8384984bbf
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-09-30 19:51:27 +0200

    Task finish

--- js/script.js
+++ js/script.js
@@ -2,14 +2,19 @@
 
 $(function(){
 	var carouselList = $('#carousel ul');
-	$(document).changeSlide(function(){
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  });
-	setInterval(changeSlide, 3000);
 
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    }
 
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
+    setInterval(function() {
+    	changeSlide();
+    }, 3000);
+    
+	function moveFirstSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');   	
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
 });
